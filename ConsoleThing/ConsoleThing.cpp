700215e168350d4c618ee7faf6fa785213d949c6
EugW <wug.bond@gmail.com>
2022-07-07 13:54:42 +0300

    sharpening

--- ConsoleThing/ConsoleThing.cpp
+++ ConsoleThing/ConsoleThing.cpp
@@ -9,7 +9,7 @@
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 void Init();
 void InitDX();
-void InitBitmaps();
+void InitEffects();
 void GetSwapChain(HWND hwnd);
 void ParseRawInput(PRAWINPUT pRawInput);
 
@@ -17,23 +17,27 @@ HRESULT hr;
 ID3D11Device* device3D;
 ID2D1DeviceContext* context2D;
 ID2D1SolidColorBrush* white;
-DXGI_SWAP_CHAIN_DESC1 swapChainDesc = { 0 };
-IDXGISwapChain1* swapChain;
+DXGI_SAMPLE_DESC sampleDesc;
+DXGI_SWAP_CHAIN_DESC1 swapChainDesc;
 IDXGIFactory2* factoryDXGI;
-ID2D1Bitmap1* bmp[4] = {{},{},{},{}};
+D2D1_PIXEL_FORMAT format;
+D2D1_BITMAP_PROPERTIES1 bitmapProperties;
+ID2D1Effect* scaleEffect[4] = {};
+IDXGISwapChain1* swapChain;
 DXGI_PRESENT_PARAMETERS parameters = { 0, NULL, NULL, NULL };
 
 int X;
 int Y;
 int selected = 1;
 BOOL launched = FALSE;
+BOOL drawn = FALSE;
 char path[3][4096];
 char args[3][4096];
 
 int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR pCmdLine, _In_ int nCmdShow) {
     Init();
     InitDX();
-    InitBitmaps();
+    InitEffects();
     LPCSTR CLASS_NAME = "ConsoleThing";
     WNDCLASSA wc;
     ZeroMemory(&wc, sizeof(WNDCLASSA));
@@ -42,14 +46,14 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
     wc.lpszClassName = CLASS_NAME;
     wc.hCursor = NULL;
     RegisterClassA(&wc);
-    X = GetSystemMetrics(SM_CXSCREEN);
-    Y = GetSystemMetrics(SM_CYSCREEN);
-    HWND hwnd = CreateWindowExA(0, CLASS_NAME, CLASS_NAME, WS_BORDER, 0, 0, X, Y, NULL, NULL, hInstance, NULL);
+    HWND hwnd = CreateWindowExA(0, CLASS_NAME, CLASS_NAME, WS_BORDER,
+        0, 0, X, Y, NULL, NULL, hInstance, NULL);
     SetWindowLongA(hwnd, GWL_STYLE, 0);
     SetCursor(NULL);
-    ShowWindow(hwnd, nCmdShow);
+    ShowWindow(hwnd, SW_HIDE);
     MSG msg;
     ZeroMemory(&msg, sizeof(MSG));
+    PostMessage(hwnd, WM_PAINT, 0, 0);
     while (GetMessageA(&msg, NULL, 0, 0) > 0) {
         TranslateMessage(&msg);
         DispatchMessageA(&msg);
@@ -84,14 +88,20 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
             GetSwapChain(hwnd);
         }
         context2D->BeginDraw();
-        context2D->DrawBitmap(bmp[(1 + selected) * launched], D2D1::RectF(0.0f, 0.0f, X + 0.0f, Y + 0.0f), 1.0f, D2D1_INTERPOLATION_MODE_HIGH_QUALITY_CUBIC, NULL, NULL);
+        context2D->Clear(NULL);
+        context2D->DrawImage(scaleEffect[(1 + selected) * launched]);
         float onethird = X / 3.0f;
-        context2D->DrawRectangle(D2D1::RectF(selected * onethird, 0, selected * onethird + onethird, Y + 0.0f), white, !launched * 10.0f, NULL);
+        context2D->DrawRectangle(D2D1::RectF(selected * onethird + 5.0f, 5.0f,
+            selected * onethird + onethird - 5.0f, Y - 5.0f), white, !launched * 10.0f, NULL);
         hr = context2D->EndDraw();
         swapChain->Present1(1, 0, &parameters);
         if (launched) {
-            Sleep(2000);
+            Sleep(5000);
             exit(0);
+        }
+        if (!drawn) {
+            ShowWindow(hwnd, SW_NORMAL);
+            drawn = TRUE;
         }
         break;
     }
@@ -129,6 +139,13 @@ void Init() {
     rid.dwFlags = 0;
     rid.hwndTarget = 0;
     RegisterRawInputDevices(&rid, 1, sizeof(rid));
+    X = GetSystemMetrics(SM_CXSCREEN);
+    Y = GetSystemMetrics(SM_CYSCREEN);
+    sampleDesc = { 1, 0 };
+    swapChainDesc = { 0, 0, DXGI_FORMAT_B8G8R8A8_UNORM, FALSE, sampleDesc, DXGI_USAGE_RENDER_TARGET_OUTPUT,
+    2, DXGI_SCALING_NONE, DXGI_SWAP_EFFECT_FLIP_SEQUENTIAL, DXGI_ALPHA_MODE_IGNORE, 0 };
+    format = { DXGI_FORMAT_B8G8R8A8_UNORM, D2D1_ALPHA_MODE_IGNORE };
+    bitmapProperties = { format, 0, 0, D2D1_BITMAP_OPTIONS_NONE };
 }
 
 void InitDX() {
@@ -147,43 +164,31 @@ void InitDX() {
     if (deviceDXGI == NULL) {
         return;
     }
-    ID2D1Factory1* factory2D;
-    hr = D2D1CreateFactory(D2D1_FACTORY_TYPE_MULTI_THREADED, __uuidof(ID2D1Factory1), (void**)&factory2D);
+    ID2D1Factory1* factory2D = {};
+    hr = D2D1CreateFactory(D2D1_FACTORY_TYPE_MULTI_THREADED, __uuidof(ID2D1Factory1), reinterpret_cast<void**>(&factory2D));
     ID2D1Device* device2D;
     hr = factory2D->CreateDevice(deviceDXGI, &device2D);
     hr = device2D->CreateDeviceContext(D2D1_DEVICE_CONTEXT_OPTIONS_ENABLE_MULTITHREADED_OPTIMIZATIONS, &context2D);
     hr = context2D->CreateSolidColorBrush(D2D1::ColorF(255, 255, 255), &white);
-    swapChainDesc.Width = 0;
-    swapChainDesc.Height = 0;
-    swapChainDesc.Format = DXGI_FORMAT_B8G8R8A8_UNORM;
-    swapChainDesc.Stereo = false;
-    swapChainDesc.SampleDesc.Count = 1;
-    swapChainDesc.SampleDesc.Quality = 0;
-    swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
-    swapChainDesc.BufferCount = 2;
-    swapChainDesc.Scaling = DXGI_SCALING_NONE;
-    swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_SEQUENTIAL;
-    swapChainDesc.Flags = 0;
     IDXGIAdapter* adapterDXGI;
     hr = deviceDXGI->GetAdapter(&adapterDXGI);
     hr = adapterDXGI->GetParent(IID_PPV_ARGS(&factoryDXGI));
 }
 
-void InitBitmaps() {
+void InitEffects() {
     LPCSTR files[4] = {
         "ConsoleThing\\ConsoleThing.bmp",
         "ConsoleThing\\nintendo.bmp",
         "ConsoleThing\\ps.bmp",
         "ConsoleThing\\xbox.bmp"
     };
-
+    BITMAP bt = { 0 };
+    BITMAPINFO inf = { 0 };
+    HDC DC = CreateCompatibleDC(NULL);
+    ID2D1Bitmap1* tmp;
     for (int i = 0; i < 4; i++) {
-        BITMAP bt = { 0 };
-        BITMAPINFO inf = { 0 };
-        HDC DC = CreateCompatibleDC(NULL);
-        HBITMAP qwe = (HBITMAP)LoadImageA(NULL, files[i], IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
-        HBITMAP OldBitmap = (HBITMAP)SelectObject(DC, qwe);
-        GetObject(qwe, sizeof(bt), &bt);
+        HBITMAP hBitmap = (HBITMAP)LoadImageA(NULL, files[i], IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
+        GetObject(hBitmap, sizeof(bt), &bt);
         inf.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
         inf.bmiHeader.biWidth = bt.bmWidth;
         inf.bmiHeader.biHeight = -bt.bmHeight;
@@ -192,31 +197,20 @@ void InitBitmaps() {
         inf.bmiHeader.biCompression = BI_RGB;
         inf.bmiHeader.biSizeImage = ((bt.bmWidth * bt.bmBitsPixel + 31) / 32) * 4 * bt.bmHeight;
         void* pixels = malloc(inf.bmiHeader.biSizeImage);
-        GetDIBits(DC, qwe, 0, bt.bmHeight, pixels, &inf, DIB_RGB_COLORS);
-        SelectObject(DC, OldBitmap);
-        D2D1_PIXEL_FORMAT fmt = {
-            DXGI_FORMAT_B8G8R8A8_UNORM,
-            D2D1_ALPHA_MODE_IGNORE
-        };
-        D2D1_BITMAP_PROPERTIES1 bitmapProperties = {
-            fmt,
-            0,
-            0,
-            D2D1_BITMAP_OPTIONS_NONE
-        };
-        hr = context2D->CreateBitmap(D2D1::SizeU(bt.bmWidth, bt.bmHeight), pixels, bt.bmWidthBytes, bitmapProperties, &bmp[i]);
+        GetDIBits(DC, hBitmap, 0, bt.bmHeight, pixels, &inf, DIB_RGB_COLORS);
+        hr = context2D->CreateBitmap(D2D1::SizeU(bt.bmWidth, bt.bmHeight), pixels, bt.bmWidthBytes, bitmapProperties, &tmp);
+        hr = context2D->CreateEffect(CLSID_D2D1Scale, &scaleEffect[i]);
+        scaleEffect[i]->SetInput(0, tmp);
+        scaleEffect[i]->SetValue(D2D1_SCALE_PROP_SCALE, D2D1::Vector2F((float)X / bt.bmWidth, (float)Y / bt.bmHeight));
+        scaleEffect[i]->SetValue(D2D1_SCALE_PROP_SHARPNESS, 1.0f);
+        scaleEffect[i]->SetValue(D2D1_SCALE_PROP_INTERPOLATION_MODE, D2D1_SCALE_INTERPOLATION_MODE_HIGH_QUALITY_CUBIC);
         free(pixels);
     }
 }
 
 void GetSwapChain(HWND hwnd) {
     hr = factoryDXGI->CreateSwapChainForHwnd(device3D, hwnd, &swapChainDesc, NULL, NULL, &swapChain);
-    ID3D11Texture2D* backBuffer;
-    hr = swapChain->GetBuffer(0, IID_PPV_ARGS(&backBuffer));
-    D2D1_BITMAP_PROPERTIES1 bitmapProperties = { };
     bitmapProperties.bitmapOptions = D2D1_BITMAP_OPTIONS_TARGET | D2D1_BITMAP_OPTIONS_CANNOT_DRAW;
-    bitmapProperties.pixelFormat.format = DXGI_FORMAT_B8G8R8A8_UNORM;
-    bitmapProperties.pixelFormat.alphaMode = D2D1_ALPHA_MODE_IGNORE;
     IDXGISurface* backBufferDXGI;
     hr = swapChain->GetBuffer(0, IID_PPV_ARGS(&backBufferDXGI));
     if (backBufferDXGI == NULL) {
@@ -245,7 +239,8 @@ void ParseRawInput(PRAWINPUT pRawInput) {
 
     //process nav
     ULONG val = 0;
-    rt = HidP_GetUsageValue(HidP_Input, 1, 0, 0x39, &val, pPreparsedData, (PCHAR)pRawInput->data.hid.bRawData, pRawInput->data.hid.dwSizeHid);
+    rt = HidP_GetUsageValue(HidP_Input, 1, 0, 0x39, &val, pPreparsedData,
+        (PCHAR)pRawInput->data.hid.bRawData, pRawInput->data.hid.dwSizeHid);
     if (rt != HIDP_STATUS_SUCCESS) {
         return;
     }
@@ -276,7 +271,8 @@ void ParseRawInput(PRAWINPUT pRawInput) {
     case 1: {
         STARTUPINFOA info = { sizeof(info) };
         PROCESS_INFORMATION processInfo;
-        CreateProcessA(path[selected], args[selected], NULL, NULL, TRUE, 0, NULL, NULL, &info, &processInfo);
+        CreateProcessA(path[selected], args[selected], NULL, NULL,
+            TRUE, 0, NULL, NULL, &info, &processInfo);
         CloseHandle(processInfo.hProcess);
         CloseHandle(processInfo.hThread);
         launched = TRUE;
